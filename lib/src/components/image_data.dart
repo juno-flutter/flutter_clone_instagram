5a217c353ddb939a95870c9ec72a2d07ab468d19
juno-flutter <juno.flutter@gmail.com>
2022-09-18 00:04:54 +0900

    ...

--- lib/src/components/image_data.dart
+++ lib/src/components/image_data.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
+// import 'package:get/get_core/src/get_main.dart';
 
 class ImageData extends StatelessWidget {
-  String icon;
+  final String icon;
   final double? width;
 
-  ImageData(this.icon, {Key? key, this.width = 55}) : super(key: key);
+  const ImageData(this.icon, {Key? key, this.width = 55}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
