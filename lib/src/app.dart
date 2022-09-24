59636fc88b642bce566406bea53cf95e84caf4f7
juno-flutter <juno.flutter@gmail.com>
2022-09-24 12:15:32 +0900

    ...

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_clone_instagram/src/components/image_data.dart';
 import 'package:flutter_clone_instagram/src/controller/bottom_nav_controller.dart';
+import 'package:flutter_clone_instagram/src/pages/home.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:get/get_state_manager/src/simple/get_view.dart';
 
@@ -10,19 +11,22 @@ class App extends GetView<BottomNavController> {
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-      onWillPop: controller.gotoBack,
+      onWillPop: controller.goBack,
       child: Obx(
         () => Scaffold(
-          appBar: AppBar(),
-          body: IndexedStack(
-            index: controller.pageIndex.value,
-            children: const [
-              Center(child: Text('HOME')),
-              Center(child: Text('SEARCH')),
-              Center(child: Text('UPLOAD')),
-              Center(child: Text('ACTIVITY')),
-              Center(child: Text('MYPAGE')),
-            ],
+          body: Center(
+            child: Center(
+              child: IndexedStack(
+                index: controller.pageIndex.value,
+                children: const [
+                  Home(),
+                  Center(child: Text('SEARCH')),
+                  Center(child: Text('UPLOAD')),
+                  Center(child: Text('ACTIVITY')),
+                  Center(child: Text('MYPAGE')),
+                ],
+              ),
+            ),
           ),
           bottomNavigationBar: BottomNavigationBar(
             type: BottomNavigationBarType.shifting,
